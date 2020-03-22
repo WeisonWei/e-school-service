08dc87a2c32261509a32ba43608368b82f9d1e43
WeisonWei <weixx3@126.com>
2020-03-22 23:32:55 +0800

    update

--- consul-server-star.sh
+++ consul-server-star.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 cd /usr/local/bin/
-/Applications/Firefox.app/Contents/MacOS/firefox http://localhost:8500
+/Applications/Firefox.app/Contents/MacOS/firefox http://localhost:8500/ui
+# 获取服务列表
+#curl -X GET http://localhost:8500/v1/agent/services
+# 手动注册服务
+#curl -X PUT http://localhost:8500/v1/agent/service/register
+# 手动注销服务
+#curl -X PUT http://localhost:8500/v1/agent/service/deregister/my-service-id
+# 启动服务端开发者模式
 consul agent -dev
