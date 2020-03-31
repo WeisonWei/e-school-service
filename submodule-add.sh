a6c31ba87d3dfd229a9ad459f84e263b00ba3828
WeisonWei <weixx3@126.com>
2020-03-31 20:17:05 +0800

    add feign module

--- submodule-add.sh
+++ submodule-add.sh
@@ -14,7 +14,7 @@ git submodule add https://github.com/WeisonWei/e-school-service-api.git
 git submodule add https://github.com/WeisonWei/e-school-service-base.git
 git submodule add https://github.com/WeisonWei/e-school-service-task.git
 git submodule add https://github.com/WeisonWei/e-school-service-gateway.git
-
+git submodule add https://github.com/WeisonWei/e-school-service-feign.git
 #submodule add 执行完后执行
 #git submodule init
 #git submodule update
