8e9857ffb06d56c80695d98307a01c42dd499a23
WeisonWei <weixx3@126.com>
2020-04-02 14:56:38 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # e-school-service
 e-school-service
+
+# elk log 收集
+# gateway 拦截
+# account
+# task
---
+++ service-docker/middleware/ekl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker pull sebp/elk
+docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -p 5601:5601 -p 5044:5044 -p 9200:9200 -p 9300:9300 -it --name elk 2fbf0a30426d
